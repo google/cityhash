c005eee98eeb32569cad911db156a148ef72de58
gpike@google.com <gpike@google.com@325e1591-7dc4-fa6a-e451-e4e085be5db0>
2013-06-17 19:48:13 +0000

    Add NetBSD-specific bswap; update NEWS for this and the previous commit

--- trunk/src/city.cc
+++ trunk/src/city.cc
@@ -60,6 +60,15 @@ static uint32 UNALIGNED_LOAD32(const char *p) {
 #define bswap_32(x) OSSwapInt32(x)
 #define bswap_64(x) OSSwapInt64(x)
 
+#elif defined(__NetBSD__)
+
+#include <sys/types.h>
+#include <machine/bswap.h>
+#if defined(__BSWAP_RENAME) && !defined(__bswap_32)
+#define bswap_32(x) bswap32(x)
+#define bswap_64(x) bswap64(x)
+#endif
+
 #else
 
 #include <byteswap.h>
